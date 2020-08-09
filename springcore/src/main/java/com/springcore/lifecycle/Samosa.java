9037be6d35b331be94bb21f7b9700dff17016a20
karun <karunahuja1998@gmail.com>
2020-08-09 14:16:38 +0530

    implementing lifecycle methods using xml

---
+++ springcore/src/main/java/com/springcore/lifecycle/Samosa.java
@@ -0,0 +1,41 @@
+package com.springcore.lifecycle;
+
+public class Samosa {
+
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		
+		System.out.print("Setting price");
+		this.price = price;
+	}
+
+	public Samosa(double price) {
+		super();
+		this.price = price;
+	}
+
+	public Samosa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Samosa [price=" + price + "]";
+	}
+	
+	
+	public void init() {
+		System.out.print("inside init");
+	}
+	public void destroy() {
+		System.out.print("inside destroy");
+	}
+	
+	
+}
