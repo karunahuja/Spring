a9fc2e646c295a40c0befe765d320d24837ea936
karun <karunahuja1998@gmail.com>
2020-08-10 17:57:49 +0530

    Spring Expression Language | SpEL | what is SpEL | How to use Expression

---
+++ springcore/src/main/java/com/springcore/spel/Test.java
@@ -0,0 +1,28 @@
+package com.springcore.spel;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import com.springcore.stereotype.Student;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/spel/config.xml");
+		Demo d1=context.getBean("demo",Demo.class);
+	System.out.println(d1);
+	
+	
+SpelExpressionParser temp=new SpelExpressionParser();
+Expression expression=temp.parseExpression("24+54");
+
+		System.out.println(expression.getValue());
+	}
+
+}
